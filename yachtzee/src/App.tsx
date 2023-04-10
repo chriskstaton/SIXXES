7de56b4953e7e58dec784b37fb1ce874468cc2a6
Chris Staton <chris.k.staton@gmail.com>
2023-04-10 13:49:54 -0500

    styles and layout for dice

--- yachtzee/src/App.tsx
+++ yachtzee/src/App.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 
+import Dice1 from "../src/images/dice1.svg";
+import Dice2 from "../src/images/dice2.svg";
+import Dice3 from "../src/images/dice3.svg";
+import Dice4 from "../src/images/dice4.svg";
+import Dice5 from "../src/images/dice5.svg";
+import Dice6 from "../src/images/dice6.svg";
+
 function App() {
 	const [count, setCount] = useState(0);
 
@@ -8,12 +15,21 @@ function App() {
 		<div className="App">
 			<div></div>
 			<h1>Yachtzee Dice Roller</h1>
+
+			<div className="container">
+				<div className="square"></div>
+				<div className="square"></div>
+				<div className="square"></div>
+				<div className="square"></div>
+				<div className="square"></div>
+				<div className="square"></div>
+			</div>
+
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
 			</div>
-			<div className="container"></div>
 		</div>
 	);
 }
