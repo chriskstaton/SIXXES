aa5490088382c9a83e77fa65f1fe1d6d0d255c41
Chris Staton <chris.k.staton@gmail.com>
2023-05-09 10:31:16 -0400

    fix border radius inconsistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.scss";
 import DiceRoller from "./DiceRoller";
-import Scoreboard from "./Scoreboard";
 import Tips from "./Tips";
 
 function App() {
 	return (
 		<div className="App">
 			<DiceRoller />
-			<Scoreboard />
 			<Tips />
 		</div>
 	);
