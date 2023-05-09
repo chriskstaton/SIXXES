6d9f0dda4de40d34bdc94ad6a0d20c820fad2a4c
Chris Staton <chris.k.staton@gmail.com>
2023-05-09 12:07:29 -0400

    fix typos

--- src/Scoreboard.tsx
+++ src/Scoreboard.tsx
@@ -39,7 +39,6 @@ function Scoreboard(diceValues: []) {
 					</label>
 				</div>
 				<div className="upper-item">
-					{" "}
 					<label>
 						Threes
 						<input
@@ -52,7 +51,6 @@ function Scoreboard(diceValues: []) {
 					</label>
 				</div>
 				<div className="upper-item">
-					{" "}
 					<label>
 						Fours
 						<input
@@ -65,7 +63,6 @@ function Scoreboard(diceValues: []) {
 					</label>
 				</div>
 				<div className="upper-item">
-					{" "}
 					<label>
 						Fives
 						<input
@@ -78,7 +75,6 @@ function Scoreboard(diceValues: []) {
 					</label>
 				</div>
 				<div className="upper-item">
-					{" "}
 					<label>
 						Sixes
 						<input
