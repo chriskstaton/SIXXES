af1cfbabdb67371306afafb1e49926235ca03a86
Chris Staton <chris.k.staton@gmail.com>
2023-05-18 22:32:13 -0500

    [build] add build time & sha+commit to production

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useRef } from "react";
 import DiceRoller from "./DiceRoller";
 import SixesHeader from "./SixesHeader";
 import "./App.scss";
+import { github, abbreviatedSha, authorDate, commitMessage } from "~build/info";
+import time from "~build/time";
 
 function App() {
 	const headerScrollElement = useRef(null);
@@ -14,6 +16,10 @@ function App() {
 			top: ref.current.offsetTop,
 			behavior: "smooth",
 		});
+	};
+
+	const openInNewTab = (url: string | undefined) => {
+		window.open(url, "_blank", "noreferrer");
 	};
 
 	return (
@@ -28,6 +34,14 @@ function App() {
 				headerScrollElement={headerScrollElement}
 				scoreboardScrollElement={scoreboardScrollElement}
 			/>
+			<div
+				className="github"
+				onClick={() => openInNewTab(github ? github : undefined)}
+			>
+				github
+			</div>
+			<div className="commit">{abbreviatedSha + " - " + commitMessage}</div>
+			<div className="author-date">{time.toString()}</div>
 		</div>
 	);
 }
